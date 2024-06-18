eec91678805b585c7215dbc810987e3db8923282
Babaylov7 <babaylov-ayu@mail.ru>
2024-06-18 13:33:34 +0500

    2

--- app/src/main/java/com/example/victory_group_test_task/MainActivity.kt
+++ app/src/main/java/com/example/victory_group_test_task/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.victory_group_test_task
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
 import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
 import com.yandex.mapkit.mapview.MapView
 
 class MainActivity : AppCompatActivity() {
@@ -15,17 +21,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
         MapKitFactory.initialize(this)
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapview)
-
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+        requestLocationPermission()
+        val mapKit = MapKitFactory.getInstance()
+        mapView.mapWindow.map.move(
+            CameraPosition(
+                Point(56.833742, 60.635716),
+                /* zoom = */ 17.0f,
+                /* azimuth = */ 0.0f,
+                /* tilt = */ 0.0f
+            )
+        )
+        checkGPS()
+        val locationOnMapKit = mapKit.createUserLocationLayer(mapView.mapWindow)
+        locationOnMapKit.isVisible = true
     }
 
     override fun onStart() {
@@ -39,4 +50,31 @@ class MainActivity : AppCompatActivity() {
         MapKitFactory.getInstance().onStop()
         super.onStop()
     }
+
+    private fun requestLocationPermission(){
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION),
+                0)
+            return
+        }
+    }
+
+    private fun isGPSEnabled(): Boolean {
+        val lm = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+
+    private fun checkGPS(){
+        if(!isGPSEnabled()){
+            DialogManager.locationSettingsDialog(this, object : DialogManager.Listener{
+                override fun onClick() {
+                    startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                }
+            })
+        }
+    }
+
 }
